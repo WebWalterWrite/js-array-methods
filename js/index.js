82ba7a2904d44a92b476ea27d8a5b9f952e008ca
WebWalterWrite <webwalterwrite@gmail.com>
2019-02-28 17:15:15 +0100

    type: fn(create/update) desc: ajout fn displayStars et update fn event document

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
-import { OneCity, AllCities, ClearCities, MostPopulateCities, AllClothes, AddToCart, SumCart } from './fn.js';
+import { OneCity, AllCities, ClearCities, MostPopulateCities, AllClothes, AddToCart, SumCart, displayStars, clickEvent } from './fn.js';
+import { idSelector } from './utils.js';
 
 window.addEventListener("DOMContentLoaded", () => {
 
 // identifier elem
-  const cities_btn = document.querySelector("#_cities_btn");
-  const cities_clear_btn = document.querySelector("#_cities_clear");
-  const clothing_contains = document.querySelector('#_clothing')
-  const city_btn = document.querySelector("#_city_btn");
+  const cities_btn = idSelector("_cities_btn");
+  const cities_clear_btn = idSelector("_cities_clear");
+  const clothing_contains = idSelector('_clothing')
+  const city_btn = idSelector("_city_btn");
   const city_input = document.querySelector('input[name="city"]');
   const city_data = document.querySelectorAll('._city_data');
-  const cart_total_btn = document.querySelector('#_btn_total_cart');
+  const cart_total_btn = idSelector('_btn_total_cart');
 
   cities_btn.addEventListener('click', () => AllCities());
   cities_clear_btn.addEventListener('click', () => ClearCities());
@@ -19,13 +20,22 @@ window.addEventListener("DOMContentLoaded", () => {
   [...city_data].map(el => el.addEventListener('click', () => MostPopulateCities(el.dataset.int)));
 
 // Ecouter click event sur boutton add to cart
-  document.body.addEventListener('click', el => el.target.dataset.id && AddToCart(el.target.dataset.id, 'cart'));
+  document.body.addEventListener('click', (e) => clickEvent(e));
   
   
-  // Array reduce
+  /*
+    ARRAY REDUCE
+  */
   clothing_contains.innerHTML= AllClothes();
+
   cart_total_btn.addEventListener('click', () => SumCart('cart') );
 
-  // clear local storage
-  localStorage.clear();
+  localStorage.clear();  // clear local storage
+
+
+  /*
+    ARRAY STARS
+  */
+  displayStars();   // initialize Stars array
+
 });
