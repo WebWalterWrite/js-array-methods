6d50c2b02c82696ee2d8f145f2a489630464c449
WebWalterWrite <webwalterwrite@gmail.com>
2019-04-09 17:21:39 +0200

    type: data desc: ajout array Sneakers & html content (findexemple)

--- js/data.js
+++ js/data.js
@@ -9,70 +9,128 @@ export const UsCity = [
 
 export const UsCityData = [
   {
-      city: "New York",
-      population: "8622698"
+    city: "New York",
+    population: "8622698"
   },
   {
-      city: "Miami",
-      population: "463347"
+    city: "Miami",
+    population: "463347"
   },
   {
-      city: "Los Angeles",
-      population: "3999759"
+    city: "Los Angeles",
+    population: "3999759"
   },
   {
-      city: "Chicago",
-      population: "2716450"
+    city: "Chicago",
+    population: "2716450"
   },
   {
-      city: "Washington",
-      population: "693972"
+    city: "Washington",
+    population: "693972"
   },
   {
-      city: "Seattle",
-      population: "724745"
+    city: "Seattle",
+    population: "724745"
   },
   {
-      city:"Springfield",
-      population: "114868"
+    city: "Springfield",
+    population: "114868"
   },
   {
-      city:"San Bernardino",
-      population:"216995"
+    city: "San Bernardino",
+    population: "216995"
   }
 ];
 
-
 export const Clothings = [
-    {   
-        id:1,
-        product:"👕",
-        price: 9.99,
-        name: 'Tee-Shirt'
-    },
-    {
-        id:2,
-        product:"👟",
-        price: 80,
-        name: 'Running Shoe'
-    },
-    {
-        id:3,
-        product:"👖",
-        price: 39.99,
-        name: 'Jeans'
-    },
-    {
-        id:4,
-        product:"🧢",
-        price: 12,
-        name: 'Casquette'
-    },
-    {
-        id:5,
-        product:"🎒",
-        price: 29.99,
-        name: 'BackPack'
-    },
+  {
+    id: 1,
+    product: "👕",
+    price: 9.99,
+    name: "Tee-Shirt"
+  },
+  {
+    id: 2,
+    product: "👟",
+    price: 80,
+    name: "Running Shoe"
+  },
+  {
+    id: 3,
+    product: "👖",
+    price: 39.99,
+    name: "Jeans"
+  },
+  {
+    id: 4,
+    product: "🧢",
+    price: 12,
+    name: "Casquette"
+  },
+  {
+    id: 5,
+    product: "🎒",
+    price: 29.99,
+    name: "BackPack"
+  }
+];
 
-]
+export const Sneakers = [
+  {   
+      brand: 'Nike',
+      data: [
+      { picture: "nike1", name: "EPIC REACT FLYKNIT 2", price: 90},
+      { picture: "nike2", name: "AIR SKYLON II", price: 70},
+      { picture: "nike2", name: "AIR MAX 1", price: 135}
+    ]
+  },
+  { 
+      brand: 'Addidas',
+      data: [
+      { picture: "addidas1", name: "STAN SMITH TREFOIL", price: 100},
+      { picture: "addidas2", name: "HANDBALL SPZL", price: 100 },
+      { picture: "addidas3", name: "STAN SMITH", price: 70 }
+    ]
+  },
+  {   
+      brand: 'Reebok',
+      data: [
+      { picture: "reebok1", name:'RAPIDE', price: 80 },
+      { picture: "reebok2", name:'AZTREK',price: 90 },
+      { picture: "reebok3", name:'CLASSIC LEATHER', price: 80 }
+    ]
+  }
+];
+
+export const findExemple = `
+
+const Sneakers = [
+&nbsp; {
+
+&nbsp;&nbsp; brand: 'Nike',
+&nbsp;&nbsp; data: [
+&nbsp;&nbsp;&nbsp;&nbsp; { picture: "nike1", name: "EPIC REACT FLYKNIT 2", price: 90},
+&nbsp;&nbsp;&nbsp;&nbsp; { picture: "nike2", name: "AIR SKYLON II", price: 70},
+&nbsp;&nbsp;&nbsp;&nbsp; { picture: "nike2", name: "AIR MAX 1", price: 135}
+&nbsp;&nbsp; ]
+&nbsp; },
+&nbsp; {
+
+&nbsp;&nbsp; brand: 'Addidas',
+&nbsp;&nbsp; data: [
+&nbsp;&nbsp;&nbsp;&nbsp; { picture: "addidas1", name: "STAN SMITH TREFOIL", price: 100},
+&nbsp;&nbsp;&nbsp;&nbsp; { picture: "addidas2", name: "HANDBALL SPZL", price: 100 },
+&nbsp;&nbsp;&nbsp;&nbsp; { picture: "addidas3", name: "STAN SMITH", price: 70 }
+&nbsp;&nbsp; ]
+&nbsp; },
+&nbsp; {
+    
+&nbsp;&nbsp; brand: 'Reebok',
+&nbsp;&nbsp; data: [
+&nbsp;&nbsp;&nbsp;&nbsp; { picture: "reebok1", name:'RAPIDE', price: 80 },
+&nbsp;&nbsp;&nbsp;&nbsp; { picture: "reebok2", name:'AZTREK',price: 90 },
+&nbsp;&nbsp;&nbsp;&nbsp; { picture: "reebok3", name:'CLASSIC LEATHER', price: 80 }
+&nbsp; ]
+&nbsp; }
+];
+`;
