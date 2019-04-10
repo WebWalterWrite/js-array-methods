6801905acef26d4b8e729809a7079fe80081f2a7
WebWalterWrite <webwalterwhrite@gmail.com>
2019-04-10 22:02:29 +0200

    desc: ajout array Sneakers

--- js/data.js
+++ js/data.js
@@ -81,7 +81,7 @@ export const Sneakers = [
       data: [
       { picture: "nike1", name: "EPIC REACT FLYKNIT 2", price: 90},
       { picture: "nike2", name: "AIR SKYLON II", price: 70},
-      { picture: "nike2", name: "AIR MAX 1", price: 135}
+      { picture: "nike3", name: "AIR MAX 1", price: 135}
     ]
   },
   { 
@@ -95,42 +95,11 @@ export const Sneakers = [
   {   
       brand: 'Reebok',
       data: [
-      { picture: "reebok1", name:'RAPIDE', price: 80 },
-      { picture: "reebok2", name:'AZTREK',price: 90 },
-      { picture: "reebok3", name:'CLASSIC LEATHER', price: 80 }
+      { picture: "reebook1", name:'RAPIDE', price: 80 },
+      { picture: "reebook2", name:'AZTREK',price: 90 },
+      { picture: "reebooK3", name:'CLASSIC LEATHER', price: 80 }
     ]
   }
 ];
 
-export const findExemple = `
 
-const Sneakers = [
-&nbsp; {
-
-&nbsp;&nbsp; brand: 'Nike',
-&nbsp;&nbsp; data: [
-&nbsp;&nbsp;&nbsp;&nbsp; { picture: "nike1", name: "EPIC REACT FLYKNIT 2", price: 90},
-&nbsp;&nbsp;&nbsp;&nbsp; { picture: "nike2", name: "AIR SKYLON II", price: 70},
-&nbsp;&nbsp;&nbsp;&nbsp; { picture: "nike2", name: "AIR MAX 1", price: 135}
-&nbsp;&nbsp; ]
-&nbsp; },
-&nbsp; {
-
-&nbsp;&nbsp; brand: 'Addidas',
-&nbsp;&nbsp; data: [
-&nbsp;&nbsp;&nbsp;&nbsp; { picture: "addidas1", name: "STAN SMITH TREFOIL", price: 100},
-&nbsp;&nbsp;&nbsp;&nbsp; { picture: "addidas2", name: "HANDBALL SPZL", price: 100 },
-&nbsp;&nbsp;&nbsp;&nbsp; { picture: "addidas3", name: "STAN SMITH", price: 70 }
-&nbsp;&nbsp; ]
-&nbsp; },
-&nbsp; {
-    
-&nbsp;&nbsp; brand: 'Reebok',
-&nbsp;&nbsp; data: [
-&nbsp;&nbsp;&nbsp;&nbsp; { picture: "reebok1", name:'RAPIDE', price: 80 },
-&nbsp;&nbsp;&nbsp;&nbsp; { picture: "reebok2", name:'AZTREK',price: 90 },
-&nbsp;&nbsp;&nbsp;&nbsp; { picture: "reebok3", name:'CLASSIC LEATHER', price: 80 }
-&nbsp; ]
-&nbsp; }
-];
-`;
